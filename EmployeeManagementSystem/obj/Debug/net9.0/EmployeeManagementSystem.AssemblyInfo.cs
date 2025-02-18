6cd2dec2425def662aa63d14e1d8abb526d5b90b
Ved <ved>
2025-02-18 18:12:39 +0530

    j

--- EmployeeManagementSystem/obj/Debug/net9.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ EmployeeManagementSystem/obj/Debug/net9.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b934f88dfd6495108036c2bf2f80ca5b56ca6cae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+759e31228db2853cac0a722c55b09ce8734f6abb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
